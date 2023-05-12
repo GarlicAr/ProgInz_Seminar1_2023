7ec40d2fdc1cdd03d7aad01d478298d94b4eb719
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-05-12 10:06:33 +0300

    Added Abstract Methods

---
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -0,0 +1,24 @@
+package lv.venta.repo;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.modules.Product;
+
+public interface IProductRepo extends CrudRepository<Product, Integer>{
+	
+	//SELECT * FROM product_table WHERE price < var(float price);
+	ArrayList<Product> findByPriceLessThan(float price);
+	
+	//SELECT * FROM product_table WHERE title= var(String title)
+	ArrayList<Product> findByTitle(String title);
+	
+	//daudzums liealks par 10 bet cena mazak par 4
+	//SELECT * FROM product_table WHERE quantity > (int Quantity) AND price < (float price)
+	ArrayList<Product> findByQuantityGreaterThanAndPriceLessThan(int Quantity, float price);
+
+	//@Query(nativeQuery = true)
+	//ArrayList<Product> funcNameDoesNotMatter();
+}
